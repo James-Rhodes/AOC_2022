a60b332e46a752cb00028f4d8700bc599ae08e00
James Rhodes <james.rhodes@hotmail.com.au>
2022-12-03 16:49:56 +1100

    Completed both tasks

---
+++ src/elf.rs
@@ -0,0 +1,5 @@
+#[derive(Debug)]
+pub struct Elf {
+    pub snack_calories: Vec<i32>,
+    pub total_calories: i32,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use std::fs;
+mod elf;
+
 fn main() {
     println!("Hello world");
+    let input_text =
+        fs::read_to_string("./../input/input.txt").expect("The file could not be read or found!");
+
+    let mut all_elves: Vec<elf::Elf> = Vec::new();
+
+    let mut current_elf: elf::Elf = elf::Elf {
+        snack_calories: Vec::new(),
+        total_calories: 0,
+    };
+    let mut largest_calories = 0;
+    for line in input_text.split("\n") {
+        let num: i32 = match line[0..(line.len() - 1)].parse() {
+            Ok(n) => n,
+            _ => -1,
+        };
+
+        if num != -1 {
+            current_elf.snack_calories.push(num);
+            current_elf.total_calories += num;
+        } else {
+            if largest_calories < current_elf.total_calories {
+                largest_calories = current_elf.total_calories;
+            }
+
+            all_elves.push(current_elf);
+            current_elf = elf::Elf {
+                snack_calories: Vec::new(),
+                total_calories: 0,
+            };
+        }
+    }
+
+    println!("{:?}", all_elves);
+
+    // PART ONE ANSWER
+    println!("Largest number of calories = {}", largest_calories);
+
+    all_elves.sort_by(|a, b| b.total_calories.cmp(&a.total_calories));
+    let mut top_three_total_calories = 0;
+    for i in 0..3 {
+        top_three_total_calories += all_elves[i].total_calories;
+    }
+
+    // PART TWO ANSWER
+    println!("{}", top_three_total_calories);
 }
